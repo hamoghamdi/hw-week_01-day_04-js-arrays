d36f02d0f778bc7c89a8e37f6459baf3c24c33bc
hamoghamdi <haneen.aljabrah@gmail.com>
2019-04-24 20:15:50 +0300

    Problem 6 and Problem 10 Solved.

--- homework.js
+++ homework.js
@@ -8,7 +8,6 @@ const summation = function (num){
 }
 summation(5);
 
-
 // Problem 2
 const summationEven = function (num){
     let sum = 0;
@@ -30,7 +29,6 @@ const avg = function (numbers)
     let avrg = sum / numbers.length ;
     return avrg;
 }
-
 avg([8, 2, 2, 4]);
 
 // Problem 4
@@ -40,8 +38,7 @@ const reversse = function (txt){
     txtArray = txtArray.join("");
     return txtArray;
 }
-
-reversse("Hello");
+reversse("caterpillar");
 
 // Problem 5
 const addDashes = function (txtArray){
@@ -49,3 +46,55 @@ const addDashes = function (txtArray){
     return txtArray;
 }
 addDashes(['test1', 'test2', 'test3']);
+
+// Problem 6
+const countUpAndDown = function (top){
+    let arr =[];
+
+    for (let i=1; i<=top; i++){
+        arr.push(i);
+    } 
+    for (let i = 2; i<top && i>0; i--) {
+        arr.push(i);
+    }
+    return arr;
+} 
+countUpAndDown(3);
+
+// Problem 7
+// NO IDEA 
+// charAt ???, search for a character in an array of words js
+const wordsWithA = function (arr1){
+    let arr2 = [];
+    for (let i=0; i<arr1.length; i++){
+        
+    }
+}
+
+// problem 8
+// same as 7
+const wordsWithLetter = function(){}
+
+// Problem 9
+const longestWord = function (){
+
+}
+
+// Problem 10 
+const largestEvenNumber = function(numbers){
+    let numArr = [];
+    for (let i=0; i<numbers.length; i++){
+        if (numbers[i]%2 === 0){
+            numArr.push(numbers[i]);
+        }
+    }
+    
+    let largNum = 0;
+    for(let i=0; i<numArr.length; i++){
+        if (numArr[i]>= largNum){
+            largNum = numArr[i];
+        }
+    }
+    return largNum;
+}
+largestEvenNumber([1, 2, 3, 10, 4, 7, 0]);
